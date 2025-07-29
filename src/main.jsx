d7cd6910bb939c4404db41988e483d6e1d6f080d
Luca Balasoiu <balasoiu2005@yahoo.com>
2025-07-29 17:49:18 +0300

    fixed roter

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { router } from './router.jsx'
-import {  RouterProvider } from 'react-router-dom'
+import { router } from './router'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
+import App from "./App";
 import Signup from "./components/Signup";
 import Signin from "./components/Signin";
 import Dashboard from "./components/Dashboard";
 
-export const routert = createBrowserRouter([
-    {path: "/", element: <Home />},
+export const router = createBrowserRouter([
+    {path: "/", element: <App />},
     {path: "/signup", element: <Signup />},
     {path: "/signin", element: <Signin />},
     {path: "/dashboard", element: <Dashboard />},
