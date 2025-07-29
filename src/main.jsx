af5eca40be93aa45e0572ce97a6610ce2b4994fc
Luca Balasoiu <balasoiu2005@yahoo.com>
2025-07-29 17:32:29 +0300

    router set up

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { router } from './router.jsx'
-import { Router, RouterProvider } from 'react-router-dom'
+import {  RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <>
-      <h1 className="text-center text-amber-300">React Supabase Auth & Context</h1>
+      <h1 className="text-center text-amber-300 pt-4 text-3xl">
+        React Supabase Auth & Context
+      </h1>
       <RouterProvider router={router} />
     </>
     
   </StrictMode>,
-)
+);
